336327d70dc9b418feab217d0521cc040919f932
Matt Eva <matteweva@gmail.com>
2021-09-30 11:48:56 -0700

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+/* const mainElement = document.getElementById('main');
+
+function removeElement(x) {
+    x.remove();
+}
+
+removeElement(mainElement); */
+
+// document.body.removeChild(main);
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id','victory');
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+document.body.append(newHeader);
